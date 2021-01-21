f8d0edfe2fe5bf26d9ef9913856d4dfd25a63543
Thaumy Cheng <Studio_ai@outlook.com>
2021-01-21 23:45:42 +0800

    Create sketch_jan21a.ino

---
+++ 伪ttl/sketch_jan21a.ino
@@ -0,0 +1,26 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial) {
+  }
+
+  Serial.println("OK");
+
+  mySerial.begin(115200);
+}
+
+void loop()
+{
+  if (mySerial.available())
+  {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available())
+  {
+   
+    mySerial.write(Serial.read());
+  }
+}
